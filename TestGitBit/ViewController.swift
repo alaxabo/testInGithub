26bd33598a48218ddaad4781af13c93fd47bf4ec
ANHTT-D1 <anhtt@rikkeisoft.com>
2018-07-30 10:28:39 +0700

    Initial Commit

---
+++ TestGitBit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGitBit
+//
+//  Created by ANHTT-D1 on 7/30/18.
+//  Copyright © 2018 RikkeiIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
